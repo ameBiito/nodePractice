134b3d44240c1a596b2f3715605fcbe5a6e7eb18
Andrew <andreabf2001@gmail.com>
2024-05-20 11:12:33 +0200

    Updated node and modified layout

--- examples/server.js
+++ examples/server.js
@@ -1,14 +1,43 @@
 import http from 'http';
-const PORT = 5000;
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
+const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) =>{
+//GEt current Path
+const __filename = url.fileURLToPath(import.meta.url); 
+const __dirname  = path.dirname(__filename);
+
+const server = http.createServer(async(req, res) => {
     //res.setHeader('Content-Type', 'text/html');
     //res.statusCode = 404; // we can shorten this 
-    res.writeHead(500, {
-        'Content-Type': 'application/json'
-    });
+    try{
+        //chck if GET request 
+        if(req.method === 'GET'){
+            let filePath;
+            if(req.url == '/'){
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if(req.url == '/about'){
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else {
+                throw new Error('Not Found');
+            }
+
+            const data = await fs.readFile(filePath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        } else {
+            throw new Error("Method not allowed");
+        }
+
+    } catch(error) {
+        res.writeHead(500, {'Content-type': 'text/plain'});
+        res.end('<h1>Server Error</h1>');
+    }
+   
     //res.write('Hello World'); //we can directly use end() for such a simple thing 
-    res.end(JSON.stringify({message: "Server Error"}));
+    //res.end("<h1>Hello World</h1>");
 });
 
 server.listen(PORT, () =>{
